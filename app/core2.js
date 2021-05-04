f484bcfd7c30a640730e5179d799ca8f19ee4753
Rahul Kumar <rahul10.pu@gmail.com>
2021-05-04 22:23:55 +0530

    require vs import

---
+++ app/core2.js
@@ -0,0 +1,11 @@
+// import 'core';
+// core.isEven(123)
+
+// const val=require('core1')
+// // require('core1')
+// // require('core1')
+// val.isEven(12)
+
+// import http from 'http';
+// const server = http.createServer();
+// server.listen(8888)
