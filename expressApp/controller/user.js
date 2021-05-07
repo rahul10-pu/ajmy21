59bbe7b41abe364dc0644f9fd2e327105b2ac0a0
Rahul Kumar <rahul10.pu@gmail.com>
2021-05-07 22:32:51 +0530

    added post, delete, patch request using express

--- expressApp/controller/user.js
+++ expressApp/controller/user.js
@@ -1,8 +1,28 @@
+import {v4 as uuid} from 'uuid';
 let users=[];
 
 export const getUsers=(req, res)=>{
     console.log("get request hit")
     res.send(users);
 }
-
-
+//uuid - Universal unique identifier
+export const saveUser=(req, res)=>{
+    const user= req.body;
+    const id=uuid()
+    users.push({...user, id: id});
+    // console.log(users)
+    res.send(id)
+}
+export const getUserByID=(req, res)=>{
+    const user =  users.find((user)=> user.id===req.params.id)
+    res.send(user)
+}
+export const deleteByID=(req, res)=>{
+    users =  users.filter((user)=> user.id!==req.params.id)
+    res.send(req.params.id)
+}
+export const updateNameByID=(req, res)=>{
+    const user =  users.find((user)=> user.id===req.params.id)
+    user.name=req.body.name
+    res.send(user)
+}
--- expressApp/routes/user.js
+++ expressApp/routes/user.js
@@ -1,11 +1,16 @@
 import express from 'express';
 
-import {getUsers} from '../controller/user.js';
+import {getUsers, saveUser, getUserByID, deleteByID, updateNameByID} from '../controller/user.js';
 
 const router=express.Router();
 
 router.get("/",getUsers)
-// router.post("/", saveUser)
+router.get("/:id",getUserByID)
+router.post("/", saveUser)
+router.delete("/:id", deleteByID)
+router.patch("/:id", updateNameByID)
+// router.put("/:id", updateByID)
+
 
 // router.get("/id",getUsersById)
 
